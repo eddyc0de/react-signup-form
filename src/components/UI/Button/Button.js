ab0e7afa79d8aa22c5f239de123d9991b7aba622
eddyc0de <edgar.pedreno@gmail.com>
2020-08-16 10:16:07 +0200

    Refactor button component and styles

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-const Button = props => {
-  return <button className={styles.Button} btnstyle={props.btnstyle}>{props.children}</button>;
-};
+const Button = props => (
+  <button className={styles.Button} btnstyle={props.btnstyle}>{props.children}</button>
+);
 
 export default Button;
