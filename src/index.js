b66b3a9feb05a8dc7c342816490820c4fb99724c
eddyc0de <edgar.pedreno@gmail.com>
2020-08-16 10:15:27 +0200

    Add Redux

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import authReducer from './store/reducers/auth';
 
-/* const rootReducer = combineReducers({
+const rootReducer = combineReducers({
   auth: authReducer
 });
 
-const store = createStore(rootReducer); */
+//Activates Redux DevTools
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  //<Provider store={store}>
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-  //</Provider>,
+  </Provider>,
   document.getElementById('root')
 );
 
