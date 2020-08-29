38996f875e44ad53f254bfb0096bd937dc5b0812
eddyc0de <edgar.pedreno@gmail.com>
2020-08-29 11:58:18 +0200

    Refactor state code

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,28 +1,39 @@
 import * as actionTypes from './actionTypes';
 import axios from 'axios';
 
-export const authSignUpSuccess = (errorMessage) => {
+export const authSignUpSuccess = (responseMessage) => {
   return {
     type: actionTypes.AUTH_SIGNUP_SUCCESS,
-    errorMessage: errorMessage
+    error: false,
+    responseMessage: responseMessage
   };
 }
 
-export const authSignUpFail = (errorMessage) => {
+export const authSignUpFail = (responseMessage) => {
   return {
     type: actionTypes.AUTH_SINGUP_FAIL,
-    errorMessage: errorMessage
+    error: true,
+    responseMessage: responseMessage
   };
 }
+
+export const showLoaderSpinner = (isLoading) => {
+  return {
+    type: actionTypes.SHOW_LOADING_SPINNER,
+    isLoading: isLoading
+  };
+};
 
 export const auth = (userData) => {
   return dispatch => {
+    dispatch(showLoaderSpinner(true));
     axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB3c60h9E5xoWgbRo8qiS78NRM5rRpzlPs', userData)
         .then(response => {
-          console.log(response);
+          dispatch(showLoaderSpinner(false));
           dispatch(authSignUpSuccess(response.statusText));
         })
         .catch(error => {
+          dispatch(showLoaderSpinner(false));
           dispatch(authSignUpFail(error.response.data.error.message));
         });
   };
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,23 +1,34 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-  errorMessage: 'init',
-  loading: false
+  responseMessage: 'init',
+  error: false,
+  isLoading: false
 }
 
 const authSignUpSuccess = (state, action) => {
   return {
     ...state,
-    errorMessage: action.errorMessage
+    error: action.error,
+    responseMessage: action.responseMessage
   };
 }
 const authSignUpFail = (state, action) => {
   return {
     ...state,
-    errorMessage: action.errorMessage
+    error: action.error,
+    responseMessage: action.responseMessage
   };
 }
 
+const showLoaderSpinner = (state, action) => {
+  return  {
+    ...state,
+    isLoading: action.isLoading
+  };
+};
+
+//Manager Function
 const reducer = (state = initialState, action) => {
   switch(action.type) {
     case actionTypes.AUTH_SIGNUP_SUCCESS:
@@ -25,6 +36,9 @@ const reducer = (state = initialState, action) => {
       
     case actionTypes.AUTH_SINGUP_FAIL:
       return authSignUpFail(state, action);
+    
+    case actionTypes.SHOW_LOADING_SPINNER:
+      return showLoaderSpinner(state, action);
       
     default:
       return state;
