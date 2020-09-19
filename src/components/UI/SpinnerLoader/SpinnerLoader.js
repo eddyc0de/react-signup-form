abbdd8217c21681ecb2b14170a4654c40ba83c52
eddyc0de <edgar.pedreno@gmail.com>
2020-09-19 11:45:17 +0200

    Added spinner loader

---
+++ src/components/UI/SpinnerLoader/SpinnerLoader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './SpinnerLoader.module.css';
+
+const SpinnerLoader = () => (
+  <div className={styles.loader}></div>
+);
+
+export default SpinnerLoader;
