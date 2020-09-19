b528533deb2f54bf78632c0fa72d638b513c940b
eddyc0de <edgar.pedreno@gmail.com>
2020-09-19 11:49:25 +0200

    Replace Layout for Fragment in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SignUpForm from './containers/SignUpForm/SignUpForm';
-import Layout from './hoc/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
-      <Layout>
+      <React.Fragment>
         <BrowserRouter>
           <Switch>
             <Route path='/' component={SignUpForm} />
           </Switch>
         </BrowserRouter>
-      </Layout>
+      </React.Fragment>
     );
   }
 }
