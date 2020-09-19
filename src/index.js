fdeead6d16fa41f3ab4434c46765ab28bded1c3c
eddyc0de <edgar.pedreno@gmail.com>
2020-09-19 11:49:34 +0200

    Comments

--- src/index.js
+++ src/index.js
@@ -12,9 +12,8 @@ const rootReducer = combineReducers({
   auth: authReducer
 });
 
-//Activates Redux DevTools
+//Enable Redux DevTools
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
-
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
